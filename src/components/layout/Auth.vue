6a01058e161f49cb9fce9a1ebb681e0893d6227b
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-03 02:38:52 +0530

    feature: added auth layout

---
+++ src/components/layout/Auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="auth-layout">
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.auth-layout {
+  min-height: 100vh;
+  background-color: #f8f9fa;
+}
+</style>
