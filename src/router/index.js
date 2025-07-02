152d86b5773fee3a31f122a237612d76a2d270db
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-03 02:41:04 +0530

    update: added auth and default layouts

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import Login from '../views/LoginView.vue'
+import Dashboard from '../views/AboutView.vue'
+
+import AuthLayout from '../components/layout/Auth.vue'
+import DefaultLayout from '../components/layout/Default.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: AuthLayout,
+    children: [{ path: '', name: 'Login', component: Login }],
+  },
+  {
+    path: '/dashboard',
+    component: DefaultLayout,
+    children: [
+      { path: '', name: 'Dashboard', component: Dashboard },
+    ],
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
