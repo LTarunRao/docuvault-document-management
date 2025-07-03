7945f0ad2c6ff84bded7744a6951d114011b43b7
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-04 00:10:06 +0530

    feat: add pinia for state management

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
-
 // Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 // Styles
 import 'unfonts.css'
@@ -20,6 +20,7 @@ import { messages } from './constants/messages'
 import { rules } from './utils/rules'
 
 const app = createApp(App)
+app.use(createPinia())
 app.config.globalProperties.$messages = messages
 app.config.globalProperties.$rules = rules
 
