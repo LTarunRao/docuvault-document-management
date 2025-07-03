a7541d8dd4e47505decc26ca9a329cd7536456b5
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-04 03:53:59 +0530

    style: add name

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        NAVABAR
-    </div>
+  <v-app-bar
+    flat
+    height="60"
+    color="white"
+    :elevation="2"
+  >
+    <v-container fluid class="d-flex align-center ">
+      <b>
+      DocuVault
+      </b>
+    </v-container>
+  </v-app-bar>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+.border-b {
+  border-bottom: 1px solid #e0e0e0;
+}
+</style>
