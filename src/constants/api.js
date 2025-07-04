ee31cd111bd1917ae8ba7d31d00d6799d9f72d89
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-04 16:05:09 +0530

    feat: add api end points

---
+++ src/constants/api.js
@@ -0,0 +1,11 @@
+export const api = {
+  auth: {
+    generateOTP: "/documentManagement//generateOTP",
+    validateOTP: "/documentManagement//validateOTP",
+  },
+  documentManagement: {
+    saveDocumentEntry: "/documentManagement//saveDocumentEntry",
+    searchDocumentEntry: "/documentManagement//searchDocumentEntry",
+    documentTags: "/documentManagement/documentTags",
+  },
+};
