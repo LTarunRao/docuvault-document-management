6e50239e3466e61fca3acda63ab78d4f657b346c
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-04 16:04:04 +0530

    feat: add api and global api call function

--- src/main.js
+++ src/main.js
@@ -18,12 +18,17 @@ import 'unfonts.css'
 
 import { messages } from './constants/messages'
 import { rules } from './utils/rules'
+import api_call from './utils/axios'
+import {api} from './constants/api'
 
 const app = createApp(App)
 app.use(createPinia())
 app.config.globalProperties.$messages = messages
 app.config.globalProperties.$rules = rules
+app.config.globalProperties.$api_call = api_call
+app.config.globalProperties.$api = api
 
 registerPlugins(app)
 
 app.mount('#app')
+// 9551221
