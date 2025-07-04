2f077384f478d5205dfbae930e0f5b943529bb65
L Tarun Rao <l.tarun.rao1@gmail.com>
2025-07-04 16:08:23 +0530

    style: update height

--- src/components/layout/Default.vue
+++ src/components/layout/Default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="default-layout">
     <Navbar />
-    <div class="container mt-4">
-      <router-view />
+    <div class="default-wrapper">
+      <router-view style="height: calc(100dvh-60px)" />
     </div>
   </div>
 </template>
@@ -10,3 +10,12 @@
 <script setup>
 import Navbar from '@/components/layout/Navbar.vue'
 </script>
+<style scoped>
+.default-layout{
+    /* min-height: 100dvh; */
+}
+.default-wrapper{
+    /* background-color: aliceblue; */
+    /* min-height: calc(100dvh-60px); */
+}
+</style>
